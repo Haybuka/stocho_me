7937104077a30bcdc97c34254d201d3648e523ba
haybuka <paschal.nduleo@gmail.com>
2023-12-22 00:36:49 +0100

    dashboard cleanup

--- src/pages/dashboard/card.jsx
+++ src/pages/dashboard/card.jsx
@@ -4,8 +4,8 @@ import CountUp from 'react-countup';
 const Card = ({ amount, title, delay }) => {
   return (
     <aside className="bg-white rounded-lg p-10 text-center lg:my-0 col-span-12 md:col-span-6 xl:col-span-4">
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="my-1 text-2xl">
+      <h3 className="">{title}</h3>
+      <p className="my-1 text-xl">
         <CountUp delay={delay} end={amount} />
       </p>
     </aside>
