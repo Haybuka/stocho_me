88b03959a9cc06e029b36fe3de135fb68cffcf00
haybuka <paschal.nduleo@gmail.com>
2023-12-22 16:27:34 +0100

    cleanup

--- src/protectedRoute/index.jsx
+++ src/protectedRoute/index.jsx
@@ -1,33 +1,36 @@
-import useAuthenticated from '../hooks/useAuthenticated';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 // eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuthenticated();
+  const token = localStorage.getItem('__token__');
   const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    navigate('/login');
-  } else {
-    return (
-      <>
-        {children}
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </>
-    );
-  }
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+      return;
+    }
+  }, []);
+
+  return (
+    <>
+      {children}
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
 };
 
 export default ProtectedRoute;
